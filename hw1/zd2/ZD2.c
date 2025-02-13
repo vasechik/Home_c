9b775702486be7f163904b8e440c0b0cd4a914a6
Vasilii <vasechik50@gmail.com>
2025-02-13 13:47:50 +0300

    первый коммит

---
+++ hw1/zd2/ZD2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+/*
+Let's
+   go
+     to walk
+*/
+	printf("Let's\n");
+	printf("   go\n");
+	printf("     to walk\n");
+	return 0;
+}
+
---
+++ hw1/zd3/ZD3.c
@@ -0,0 +1,25 @@
+
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+/*
+      *
+     ***
+    *****
+   *******
+    HHOHH
+    ZZZZZ
+ */  
+  printf("    *    \n");
+  printf("   ***   \n");
+  printf("  *****  \n");
+  printf(" ******* \n");
+  printf("  HHOHH  \n");
+  printf("  ZZZZZ  \n");
+	
+	return 0;
+}
+
